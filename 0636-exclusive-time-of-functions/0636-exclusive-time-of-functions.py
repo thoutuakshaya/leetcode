4c9c581ba317753ed807f7856b7303be1048a7a4
Akshaya Thoutu <thoutuakshaya1@gmail.com>
2026-06-01 17:26:21 +0530

    Time: 9 ms (19.36%), Space: 19.2 MB (87.86%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -0,0 +1,29 @@
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        result = [0] * n
+        stack = []
+
+        prevTime = 0
+
+        for log in logs:
+
+            fn, typ, time = log.split(":")
+            fn = int(fn)
+            time = int(time)
+
+            if typ == "start":
+
+                if stack:
+                    result[stack[-1]] += time - prevTime
+
+                stack.append(fn)
+                prevTime = time
+
+            else:
+
+                result[stack.pop()] += time - prevTime + 1
+
+                prevTime = time + 1
+
+        return result
+        

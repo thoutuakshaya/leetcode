3890a9e47f4287adeee6b31ad0f505c231411179
Akshaya Thoutu <thoutuakshaya1@gmail.com>
2026-06-02 16:07:23 +0530

    Time: 319 ms (5.23%), Space: 19.3 MB (86.05%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
@@ -1,17 +1,21 @@
 class Solution:
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
        
-        minans=[]
+        #minans=[]
+        minans=float(inf)
         for i in range(len(landStartTime)):
             for j in range (len(waterStartTime)):
                 T1=landStartTime[i]+landDuration[i]
                 t1=max(T1,waterStartTime[j])
                 T2=t1+waterDuration[j]
-                minans.append(T2)
+                #minans.append(T2)
+                minans=min(minans,T2)
         for i in range (len(waterStartTime)):
             for j in range(len(landStartTime)):
                 z1=waterStartTime[i]+waterDuration[i]
                 Z1=max(z1,landStartTime[j])
                 z2=Z1+landDuration[j]
-                minans.append(z2)
-        return min(minans)
+                #minans.append(z2)
+                minans=min(minans,z2)
+        #return min(minans)
+        return minans

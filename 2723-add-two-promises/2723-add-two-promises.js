6f773c143d714f1d04f50edfb47d7ac883eeed95
Akshaya Thoutu <thoutuakshaya1@gmail.com>
2026-07-04 15:52:34 +0530

    Time: 47 ms (85.65%), Space: 54.5 MB (28.02%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -4,7 +4,8 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const [a,b]=await Promise.all([promise1,promise2])
+    const a=await promise1
+    const b=await promise2
     return a+b
 };
 

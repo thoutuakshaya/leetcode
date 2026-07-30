341306a46104350ae27892e3f76f21195c5ad7f8
Akshaya Thoutu <thoutuakshaya1@gmail.com>
2026-07-30 17:01:54 +0530

    Time: 36 ms (98.62%), Space: 62.6 MB (40.63%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -3,34 +3,56 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        slow = fast = head
+        slow=fast=head
+        arr=[]
+        while fast:
+            fast=fast.next.next
+            arr.append(slow.val)
+            slow=slow.next
+        max_sum=0
+        while slow:
+            max_sum=max(max_sum,slow.val+arr.pop())
+            slow=slow.next
+        return max_sum
 
-        # Find middle
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
 
-        # Reverse second half
-        prev = None
-        curr = slow
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def middle(self,head:Optional[ListNode])->int:
+#         slow=fast=head
+#         i=0
+#         while fast and fast.next:
+#             slow=slow.next
+#             fast=fast.next.next
+#             i=i+1
+#         return slow,i
+#     def nreversed(self,head:Optional[ListNode])->int:
+#         prev=None
+#         curr=head
+#         while curr:
+#             no=curr.next
+#             curr.next=prev
+#             prev=curr
+#             curr=no
+#         return prev
 
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-
-        # Find maximum twin sum
-        ans = 0
-        first = head
-        second = prev
-
-        while second:
-            ans = max(ans, first.val + second.val)
-            first = first.next
-            second = second.next
-
-        return ans
+#     def pairSum(self, head: Optional[ListNode]) -> int:
+#         if head is None:
+#             return
+#         node,idx=self.middle(head)
+#         rev=self.nreversed(node)
+#         maxi=0
+#         straight=head
+#         reve=rev
+#         for i in range(idx):
+#             maxi=max(maxi,straight.val+reve.val)
+#             straight=straight.next
+#             reve=reve.next
+#         return maxi
+        

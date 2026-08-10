cfa9d8e444f8bf62305d536e251ca6c9b2473598
Akshaya Thoutu <thoutuakshaya1@gmail.com>
2026-08-10 11:30:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -1,40 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-
-        # Key not found
         if root is None:
             return None
-
-        # Search left
-        if key < root.val:
-            root.left = self.deleteNode(root.left, key)
-
-        # Search right
-        elif key > root.val:
-            root.right = self.deleteNode(root.right, key)
-
-        # Found the node
+        if key <root.val:
+            self.deleteNode(root.left,key)
+        elif key>root.val:
+            self.deleteNode(root.right,key)
         else:
-
-            # Case 1: no left child
+            #key found annat
             if root.left is None:
                 return root.right
-
-            # Case 2: no right child
             if root.right is None:
                 return root.left
-
-            # Case 3: both children exist
-            # Find smallest node in right subtree
-            temp = root.right
-
-            while temp.left:
-                temp = temp.left
-
-            # Replace current value
-            root.val = temp.val
-
-            # Delete duplicate node
-            root.right = self.deleteNode(root.right, temp.val)
-
+            else:
+                #both present 
+                temp=root.right
+                while temp.left :
+                    temp=temp.left
+                root.val=temp.val
+                root.right=self.deleteNode(root.right,temp.val)
         return root

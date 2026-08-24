0eaabc742a05d44f80ee5b295d7588507e50f1e2
Akshaya Thoutu <thoutuakshaya1@gmail.com>
2026-08-24 15:40:43 +0530

    Time: 658 ms (80.47%), Space: 31.9 MB (100%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.py
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -1,14 +1,8 @@
 class Solution:
-    def stoneGameVIII(self, stones):
-        # Calculate total prefix sum
-        total = sum(stones)
-
-        # Initially, Alice can take all stones
-        best = total
-
-        # Move backwards
-        for i in range(len(stones) - 2, 0, -1):
-            total -= stones[i + 1]
-            best = max(best, total - best)
-
+    def stoneGameVIII(self, stones: List[int]) -> int:
+        k=sum(stones)
+        best=k
+        for i in range(len(stones)-2,0,-1):
+            k-=stones[i+1]
+            best=max(best,k-best)
         return best

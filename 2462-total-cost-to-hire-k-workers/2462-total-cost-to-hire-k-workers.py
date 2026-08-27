eaecfb7fe9c4874ab09b915fcfc51d656751f1ad
Akshaya Thoutu <thoutuakshaya1@gmail.com>
2026-08-27 13:33:34 +0530

    Time: 116 ms (70.84%), Space: 29.2 MB (59.97%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
@@ -0,0 +1,42 @@
+import heapq
+
+class Solution:
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        
+        left_heap = []
+        right_heap = []
+        
+        left = 0
+        right = len(costs) - 1
+        
+        # Add first candidates
+        while left <= right and len(left_heap) < candidates:
+            heapq.heappush(left_heap, costs[left])
+            left += 1
+        
+        # Add last candidates
+        while left <= right and len(right_heap) < candidates:
+            heapq.heappush(right_heap, costs[right])
+            right -= 1
+        
+        total = 0
+        
+        for _ in range(k):
+            
+            if not right_heap or (
+                left_heap and left_heap[0] <= right_heap[0]
+            ):
+                total += heapq.heappop(left_heap)
+                
+                if left <= right:
+                    heapq.heappush(left_heap, costs[left])
+                    left += 1
+            
+            else:
+                total += heapq.heappop(right_heap)
+                
+                if left <= right:
+                    heapq.heappush(right_heap, costs[right])
+                    right -= 1
+        
+        return total

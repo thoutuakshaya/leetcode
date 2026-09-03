b13d87b34992700c673173bbd35c37a70f53a1cb
Akshaya Thoutu <thoutuakshaya1@gmail.com>
2026-09-03 22:33:24 +0530

    Time: 12 ms (87.8%), Space: 35.4 MB (51.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,4 +1,4 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        #return min(nums1)&1 or all(i&1 for i in nums1)
+        #return (min(nums1)&1) or all(i&1 for i in nums1)
         return min(nums1) % 2 == 1 or all(a % 2 == 0 for a in nums1)
